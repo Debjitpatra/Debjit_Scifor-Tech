ab9f986cbbad55a1f7619148f2a525416578d4ce
Debjit Patra <111041850+Debjitpatra@users.noreply.github.com>
2024-10-28 21:06:10 +0530

    Task_Added

---
+++ Task/Print-Variable/.js
@@ -0,0 +1,9 @@
+let firstName="Debjit"
+let lastName ="Patra"
+const email ="debjitpatra8521@gmail.com"
+let age ="23"
+
+console.log("FirstName " +firstName);
+console.log("LastName " +lastName);
+console.log("Age " +age);
+console.log("Email " +email);
