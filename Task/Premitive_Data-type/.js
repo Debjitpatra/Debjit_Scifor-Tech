35ecef6203573aa197d6f78fe2c2549bd308a327
Debjit Patra <111041850+Debjitpatra@users.noreply.github.com>
2024-10-29 21:01:42 +0530

    Task_Added

---
+++ Task/Premitive_Data-type/.js
@@ -0,0 +1,14 @@
+let name = "Debjit";
+let age = 23;
+let present = true;
+let num = null;
+let Address;
+
+console.log(name + " is the type of String");
+console.log(age + " is the type of number");
+console.log(present + " is the type of boolean");
+console.log(num + " is the type of null");
+console.log(Address + " is the type of undefined");
+
+
+
