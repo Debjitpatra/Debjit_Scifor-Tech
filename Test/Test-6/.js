6fa125d066b1e873a3c17cd51685a438e3c0b771
Debjit Patra <111041850+Debjitpatra@users.noreply.github.com>
2024-11-08 20:51:31 +0530

    Added

---
+++ Test/Test-6/.js
@@ -0,0 +1,16 @@
+let firstName = "Debjit ";
+let secoundName = "Patra";
+let result = firstName + secoundName;
+
+let a = 10;
+let b = 5;
+
+
+console.log(result);
+
+console.log("Addition:", addition = a + b);
+console.log("Subtraction:", subtraction = a - b);
+console.log("Multiplication:", multiplication = a * b);
+console.log("Division:", division = a / b);
+
+
